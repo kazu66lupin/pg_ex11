555c82786eab70b5103e28b93da0d2776771273e
kazunori <kazunori@192.168.128.100>
2017-08-29 14:35:32 +0900

    修正２

--- src/ex0303/ConvertUnicode.java
+++ src/ex0303/ConvertUnicode.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConvertUnicode {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("文字列を入力してください");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		try{
+		try {
 			String line = reader.readLine();
-			for(int i=0; i<line.length(); i++){
-				System.out.println("'" + line.charAt(i) + "	'の文字コードは" + (line.charAt(i)+0) + "です。");
+			for (int i = 0; i < line.length(); i++) {
+				System.out.println("'" + line.charAt(i) + "	'の文字コードは" + (line.charAt(i) + 0) + "です。");
 			}
-		}catch(IOException e){
+		} catch (IOException e) {
 			System.out.println(e);
 		}
 	}
